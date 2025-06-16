f5090edeea32dc859132d5cd8f4e12f6d0a69663
OliSperber <o.sperber@student.avans.nl>
2025-06-16 16:14:38 +0200

    Update3 on Main

--- camera_oakd.py
+++ camera_oakd.py
@@ -1,5 +1,5 @@
 import depthai as dai
-import time
+import cv2
 
 class CameraOakD:
     def __init__(self):
@@ -8,23 +8,13 @@ class CameraOakD:
         cam.setPreviewSize(640, 480)
         cam.setInterleaved(False)
         self.xout = self.pipeline.createXLinkOut()
-        self.xout.setStreamName("preview")
+        self.xout.setStreamName("video")
         cam.preview.link(self.xout.input)
-        self.device = dai.Device(self.pipeline)
-        self.q = self.device.getOutputQueue(name="preview", maxSize=4, blocking=False)
 
     def capture_image(self, save_path="capture.jpg"):
-        frame = None
-        for _ in range(30):  # wacht max ~1 sec op frame
-            in_frame = self.q.tryGet()
-            if in_frame is not None:
-                frame = in_frame.getCvFrame()
-                break
-            time.sleep(0.03)
-        if frame is not None:
-            import cv2
+        with dai.Device(self.pipeline) as device:
+            q = device.getOutputQueue(name="video", maxSize=1, blocking=True)
+            frame = q.get().getCvFrame()
             cv2.imwrite(save_path, frame)
+            print(f"Frame opgeslagen als {save_path}")
             return save_path
-        else:
-            print("Geen frame ontvangen")
-            return None
